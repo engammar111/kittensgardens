6447fd31a457e6c2e2f3ff824ccf2e71125cbfdd
engammar111 <34649325+engammar111@users.noreply.github.com>
2020-04-25 02:56:38 +0200

    Updates

---
+++ precache-manifest.3009c13e8083ebb19201ede066252b7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "453f17c1349324cf691346c1bf2e30a4",
+    "url": "/kittensgardens/index.html"
+  },
+  {
+    "revision": "338c4ac3cfbb5c3e37d9",
+    "url": "/kittensgardens/static/css/main.f251e27e.chunk.css"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/kittensgardens/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "338c4ac3cfbb5c3e37d9",
+    "url": "/kittensgardens/static/js/main.5a87c2c6.chunk.js"
+  },
+  {
+    "revision": "b205eb59080b0e136ceb",
+    "url": "/kittensgardens/static/js/runtime~main.57a89c11.js"
+  }
+]);
